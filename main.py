09164fb491afda9a147fdce740e6560cd7e61d74
simpleapples <zangzhiya@gmail.com>
2018-03-09 13:24:36 +0800

    Feat: complete

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from cli.command import Command
 
 def main():
     command = Command()
+
     command.start_peer('127.0.0.1', 5000)
+    command.start_peer('127.0.0.1', 5001)
+
+    import time
+    time.sleep(1)
+
+    command.connect_peer('127.0.0.1', 5000, '127.0.0.1', 5001)
+
     command.mine('127.0.0.1', 5000, 'hello')
+    command.mine('127.0.0.1', 5001, 'world')
+
     command.get_chain('127.0.0.1', 5000)
+    command.get_chain('127.0.0.1', 5001)
 
 
 if __name__ == '__main__':
